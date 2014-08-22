6934c4a38d1797de69a871da33ee172cd6c3f9c3
Robert Jenkins <bobert@gmail.com>
2014-08-21 22:28:19 -0400

    - new bad_phrase#create now redirects to bad_phras#new upon success

--- app/controllers/bad_phrases_controller.rb
+++ app/controllers/bad_phrases_controller.rb
@@ -9,7 +9,7 @@ class BadPhrasesController < ApplicationController
     begin
       bad_phrase  = Phrase.where(:value => params[:bad_phrase][:value], :positive_sentiment => false).first_or_create!
       flash[:sucess] = "Your contribution has been noted, and we thank you for your assitance in the endevaor"
-      redirect_to root_path
+      redirect_to new_bad_phrase_path
     rescue Exception => error
       @bad_phrase = Phrase.new
       flash[:error] = "Do pardon us, but #{error}"
