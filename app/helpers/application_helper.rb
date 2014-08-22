993b57baf86cbbf0dae022b079bef73901f0aff3
Robert Jenkins <bobert@gmail.com>
2014-08-22 19:45:49 -0400

    - successfully styled bad_phrases - removed admin js and css code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def punish_bad_word(string)
+    content_tag(:span, :class => 'bad_word_wrapper') do
+      content_tag(:span, :class => 'bad_word_heart') do
+        string
+      end
+    end
+  end
+
 end
