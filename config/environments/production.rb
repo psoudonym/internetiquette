9a18588e806e744e6c678bbb6b99268c17cb0414
Robert Jenkins <bobert@gmail.com>
2014-08-23 00:13:24 -0400

    - added production action_mailer default host thing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.action_mailer.default_url_options = { :host => "internetiquette.herokuapp.com" }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -31,7 +32,6 @@ Rails.application.configure do
 
   # Generate digests for assets URLs.
   config.assets.digest = true
-
   # `config.assets.precompile` has moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
