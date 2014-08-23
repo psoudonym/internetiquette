36c818aa57cdd4a2b13d1eb069fabab8219f26b9
Robert Jenkins <bobert@gmail.com>
2014-08-22 23:26:28 -0400

    - added admin column to user model - only admins can purge mappings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   validates_uniqueness_of :email
   validates_presence_of :email
 
