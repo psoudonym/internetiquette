4508e2233f0202a3a6d99ad933c95aaf9929fa56
Robert Jenkins <bobert@gmail.com>
2014-08-28 23:14:33 -0700

    - trying to automatically resize iframe

--- app/controllers/try_its_controller.rb
+++ app/controllers/try_its_controller.rb
@@ -1,2 +1,18 @@
 class TryItsController < ApplicationController
+
+  before_filter :do_stuff
+
+  def create
+  end
+
+  def show
+  end
+
+
+  private
+
+  def do_stuff
+    @url = params[:url] || "http://psmag.com/navigation/health-and-behavior/women-arent-welcome-internet-72170/"
+    render :action => :show
+  end
 end
